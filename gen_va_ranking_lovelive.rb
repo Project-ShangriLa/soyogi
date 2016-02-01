783e6c432e29f9d1e73b44f573a5c19bfa25ce6c
AKB428 <gandara016+1@gmail.com>
2016-02-02 01:12:17 +0900

    fix

--- gen_va_ranking_lovelive.rb
+++ gen_va_ranking_lovelive.rb
@@ -2,6 +2,27 @@
 require 'sequel'
 require 'optparse'
 
+@chara_image = {
+    'anju_inami' => 'http://data.akiba-net.com/sp/lovelive_aqours/thumb01.png',
+    'Rikako_Aida' => 'http://data.akiba-net.com/sp/lovelive_aqours/thumb02.png',
+    'suwananaka' => 'http://data.akiba-net.com/sp/lovelive_aqours/thumb03.png',
+    'arisakomiya' => 'http://data.akiba-net.com/sp/lovelive_aqours/thumb04.png',
+    'Saito_Shuka' => 'http://data.akiba-net.com/sp/lovelive_aqours/thumb05.png',
+    'Aikyan_' => 'http://data.akiba-net.com/sp/lovelive_aqours/thumb06.png',
+    'Kanako_tktk' => 'http://data.akiba-net.com/sp/lovelive_aqours/thumb07.png',
+    'aina_suzuki723' => 'http://data.akiba-net.com/sp/lovelive_aqours/thumb08.png',
+    'furihata_ai' => 'http://data.akiba-net.com/sp/lovelive_aqours/thumb09.png',
+    'nanjolno' => 'http://data.akiba-net.com/sp/lovelive_muse/twitter2nd_eri.jpg',
+    'Nitta_Emi' => 'http://data.akiba-net.com/sp/lovelive_muse/twitter2nd_honoka.jpg',
+    'shikaco_staff' => 'http://data.akiba-net.com/sp/lovelive_muse/twitter2nd_hanayo.jpg',
+    'aya_uchida' => 'http://data.akiba-net.com/sp/lovelive_muse/twitter2nd_kotori.jpg',
+    'pile_eric' => 'http://data.akiba-net.com/sp/lovelive_muse/twitter2nd_maki.jpg',
+    'tokui_sorangley' => 'http://data.akiba-net.com/sp/lovelive_muse/twitter2nd_nico.jpg',
+    'kusudaaina' => 'http://data.akiba-net.com/sp/lovelive_muse/twitter2nd_nozomi.jpg',
+    'rippialoha' => 'http://data.akiba-net.com/sp/lovelive_muse/twitter2nd_rin.jpg',
+    'mimori_suzuko' => 'http://data.akiba-net.com/sp/lovelive_muse/twitter2nd_umi.jpg'
+}
+
 def body()
   amazon_widget = <<EOS
   <script type="text/javascript"><!--
@@ -21,12 +42,12 @@ EOS
 <table class="table table-striped alt-table-responsive">
 <thead>
 <tr>
-<th class="col-md-1">声優全体順位</th>
-<th class="col-md-1">フォロワー数</th>
-<th class="col-md-1">画像</th>
-<th class="col-md-2">名前</th>
-<th class="hidden-xs hidden-sm col-md-5">詳細</th>
-<th class="hidden-xs hidden-sm col-md-2">アカウント名</th>
+<th class="col-xs-2 col-md-1">声優全体順位</th>
+<th class="col-xs-2 col-md-1">フォロワー数</th>
+<th class="col-xs-2 col-md-1">画像</th>
+<th class="col-xs-2 col-md-1">キャラ画像</th>
+<th class="col-xs-4 col-md-2">名前</th>
+<th class="hidden-xs hidden-sm col-md-6">詳細</th>
 </tr>
 </thead>
 <tbody>
@@ -58,12 +79,12 @@ EOS
     if target_account_list.include?(va[:screen_name])
       body_string += <<EOS
       <tr>
-       <th class="col-md-1"><p class="lead">#{i + 1}位</p></th>
-       <td class="col-md-1"><p class="lead">#{va[:follower]}</p></td>
-       <td class="col-md-1"><a href="#" onclick="javascript:window.open('https://twitter.com/#{va[:screen_name]}');"><img data-layzr="#{va[:profile_image_url].gsub(/normal/,'bigger')}"></a></td>
-       <td class="col-md-2"><p class="lead">#{va[:name]}</p></td>
-       <td class="col-md-5 hidden-xs hidden-sm">#{va[:description]}</td>
-       <td class="col-md-2 hidden-xs hidden-sm">#{va[:screen_name]}</td>
+       <th class="col-xs-2 col-md-1"><p class="lead">#{i + 1}位</p></th>
+       <td class="col-xs-2 col-md-1"><p class="lead">#{va[:follower]}</p></td>
+       <td class="col-xs-2 col-md-1"><a href="#" onclick="javascript:window.open('https://twitter.com/#{va[:screen_name]}');"><img src="#{va[:profile_image_url].gsub(/normal/,'bigger')}"></a></td>
+       <td class="col-xs-2 col-md-1"><img src="#{@chara_image[va[:screen_name]]}" width="75" height="75"></td>
+       <td class="col-xs-4 col-md-2"><p class="lead">#{va[:name]}</p></td>
+       <td class="hidden-xs hidden-sm col-md-6">#{va[:description]}</td>
       </tr>
 EOS
     end
@@ -107,11 +128,9 @@ head = <<"EOS"
 <link href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" integrity="sha256-7s5uDGW3AHqw6xtJmNNtr+OBRJUlgkNJEo78P4b0yRw= sha512-nNo+yCHEyn0smMxSswnf/OnX6/KwJuZTlNZBjauKhTK0c+zT+q5JOCx0UFhXQ6rJR9jg6Es8gPuD2uZcYDLqSw==" crossorigin="anonymous">
 <script src="https://code.jquery.com/jquery-2.2.0.min.js"></script>
 <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha256-KXn5puMvxCw+dAYznun+drMdG1IFl3agK0p/pqT9KAo= sha512-2e8qq0ETcfWRI4HJBzQiA3UoyFk6tbNyG+qSaIBZLyW9Xf3sWZHN/lxe9fTh1U45DpPf07yj94KsUHHWe4Yk1A==" crossorigin="anonymous"></script>
-<script src="http://cdnjs.cloudflare.com/ajax/libs/layzr.js/1.4.3/layzr.min.js"></script>
 
   <script>
   $(document).ready(function(){
-    var layzr = new Layzr({});
   });
   </script>
 </head>
@@ -119,6 +138,14 @@ head = <<"EOS"
 EOS
 
 footer = <<"EOS"
+<script type="text/javascript">
+  amzn_assoc_ad_type = "link_enhancement_widget";
+  amzn_assoc_tracking_id = "voiceactor-akiba-22";
+  amzn_assoc_placement = "";
+  amzn_assoc_marketplace = "amazon";
+  amzn_assoc_region = "JP";
+</script>
+<script src="//z-fe.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&Operation=GetScript&ID=OneJS&WS=1&MarketPlace=JP"></script>
 </body>
 </html>
 EOS
