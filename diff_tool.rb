e7208d5f32412aca48ef8319e77c0ca3d95c61fd
AKB428 <gandara016+1@gmail.com>
2016-12-18 13:33:45 +0900

    diff_tool 発言数対応

--- diff_tool.rb
+++ diff_tool.rb
@@ -1,21 +1,35 @@
 #
 # 指定された日時のステータスから現在までのフォロワー数増加を調べる
 #
-# bundle exe ruby diff_tool.rb 2016-01-31|less
+# bundle exe ruby diff_tool.rb -t 2016-01-31|less
 #
 #
 
 require 'sequel'
 require 'pp'
 require 'date'
+require 'optparse'
 
-target_day = Date.strptime(ARGV[0], "%Y-%m-%d")
+target_day = nil
+
+force_count = nil
+target_column = :follower
+
+opt = OptionParser.new
+Version = "1.0.0"
+
+
+opt.on('-t target_day', 'target_day'){|v| target_day = Date.strptime(v, "%Y-%m-%d")}
+opt.on('-s'){target_column = :statuses_count}
+opt.on('-f'){force_count = true}
+
+opt.parse!(ARGV)
+
 next_day = target_day + 1
-force_count = ARGV[1] == '-f'
 
 @db = Sequel.mysql2('anime_admin_development', :host=>'localhost', :user=>'root', :password=>'', :port=>'3306')
 
-status_rows = @db[:voice_actor_twitter_follwer_status].select_hash(:voice_actor_master_id, [:name, :follower])
+status_rows = @db[:voice_actor_twitter_follwer_status].select_hash(:voice_actor_master_id, [:name, target_column])
 
 hist_row = @db[:voice_actor_twitter_follwer_status_histories].where("get_date >= '#{target_day}' and get_date < '#{next_day}'").all
 
@@ -29,7 +43,7 @@ hist_row.each do |row|
   hist_row_map[vid] = {}
   next unless status_rows.has_key?(vid)
 
-  diff_map[vid] = status_rows[vid][1] - row[:follower] unless diff_map.has_key?(vid)
+  diff_map[vid] = status_rows[vid][1] - row[target_column] unless diff_map.has_key?(vid)
 
 end
 
